718fc0efc8dd9c520fb430d4bfa3330c4d2f01e2
David Turanski <dturanski@vmware.com>
2020-10-19 19:02:39 -0400

    WIP

--- stream-applications-integration-tests/src/test/java/org/springframework/cloud/stream/apps/integration/test/processor/HttpRequestProcessorTests.java
+++ stream-applications-integration-tests/src/test/java/org/springframework/cloud/stream/apps/integration/test/processor/HttpRequestProcessorTests.java
@@ -16,30 +16,11 @@
 
 package org.springframework.cloud.stream.apps.integration.test.processor;
 
-import java.net.InetAddress;
-import java.time.Duration;
-
-import okhttp3.mockwebserver.Dispatcher;
-import okhttp3.mockwebserver.MockResponse;
 import okhttp3.mockwebserver.MockWebServer;
-import okhttp3.mockwebserver.RecordedRequest;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
-import org.testcontainers.junit.jupiter.Container;
-import reactor.core.publisher.Mono;
 
 import org.springframework.cloud.stream.app.test.integration.LogMatcher;
-import org.springframework.cloud.stream.app.test.integration.StreamApps;
 import org.springframework.cloud.stream.apps.integration.test.support.KafkaStreamIntegrationTestSupport;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
-import org.springframework.web.reactive.function.client.ClientResponse;
 import org.springframework.web.reactive.function.client.WebClient;
-
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.awaitility.Awaitility.await;
-import static org.springframework.cloud.stream.app.test.integration.kafka.KafkaStreamApps.kafkaStreamApps;
 
 public class HttpRequestProcessorTests extends KafkaStreamIntegrationTestSupport {
 	private static MockWebServer server = new MockWebServer();
@@ -52,45 +33,46 @@ public class HttpRequestProcessorTests extends KafkaStreamIntegrationTestSupport
 
 	private static int sourcePort = findAvailablePort();
 
-//	@Container
-//	private static final StreamApps streamApps = kafkaStreamApps(
-//			HttpRequestProcessorTests.class.getSimpleName(), kafka)
-//					.withSourceContainer(httpSource(sourcePort))
-//					.withProcessorContainer(defaultKafkaProcessorContainerFor("http-request-processor")
-//							.withEnv("HTTP_REQUEST_URL_EXPRESSION",
-//									"'http://" + localHostAddress() + ":" + serverPort + "'")
-//							.withEnv("HTTP_REQUEST_HTTP_METHOD_EXPRESSION", "'POST'"))
-//					.withSinkContainer(
-//							defaultKafkaContainerFor("log-sink").withLogConsumer(logMatcher))
-//					.build();
-//
-//	@BeforeAll
-//	static void startServer() throws Exception {
-//		server.start(InetAddress.getLocalHost(), serverPort);
-//	}
-//
-//	@Test
-//	void get() {
-//		server.setDispatcher(new Dispatcher() {
-//			@Override
-//			public MockResponse dispatch(RecordedRequest recordedRequest) {
-//				return new MockResponse().setHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
-//						.setBody("{\"response\":\"" + recordedRequest.getBody().readUtf8() + "\"}")
-//						.setResponseCode(HttpStatus.OK.value());
-//			}
-//		});
-//
-//		await().atMost(Duration.ofSeconds(30))
-//				.until(logMatcher.verifies(log -> log.when(() -> {
-//					ClientResponse response = webClient
-//							.post()
-//							.uri("http://localhost:" + streamApps.sourceContainer().getMappedPort(sourcePort))
-//							.contentType(MediaType.TEXT_PLAIN)
-//							.body(Mono.just("ping"), String.class)
-//							.exchange()
-//							.block();
-//					assertThat(response.statusCode().is2xxSuccessful()).isTrue();
-//				}).matchesRegex(".*\\{\"response\":\"ping\"\\}")));
-//	}
+	// @Container
+	// private static final StreamApps streamApps = kafkaStreamApps(
+	// HttpRequestProcessorTests.class.getSimpleName(), kafka)
+	// .withSourceContainer(httpSource(sourcePort))
+	// .withProcessorContainer(defaultKafkaProcessorContainerFor("http-request-processor")
+	// .withEnv("HTTP_REQUEST_URL_EXPRESSION",
+	// "'http://" + localHostAddress() + ":" + serverPort + "'")
+	// .withEnv("HTTP_REQUEST_HTTP_METHOD_EXPRESSION", "'POST'"))
+	// .withSinkContainer(
+	// defaultKafkaContainerFor("log-sink").withLogConsumer(logMatcher))
+	// .build();
+	//
+	// @BeforeAll
+	// static void startServer() throws Exception {
+	// server.start(InetAddress.getLocalHost(), serverPort);
+	// }
+	//
+	// @Test
+	// void get() {
+	// server.setDispatcher(new Dispatcher() {
+	// @Override
+	// public MockResponse dispatch(RecordedRequest recordedRequest) {
+	// return new MockResponse().setHeader(HttpHeaders.CONTENT_TYPE,
+	// MediaType.APPLICATION_JSON_VALUE)
+	// .setBody("{\"response\":\"" + recordedRequest.getBody().readUtf8() + "\"}")
+	// .setResponseCode(HttpStatus.OK.value());
+	// }
+	// });
+	//
+	// await().atMost(Duration.ofSeconds(30))
+	// .until(logMatcher.verifies(log -> log.when(() -> {
+	// ClientResponse response = webClient
+	// .post()
+	// .uri("http://localhost:" + streamApps.sourceContainer().getMappedPort(sourcePort))
+	// .contentType(MediaType.TEXT_PLAIN)
+	// .body(Mono.just("ping"), String.class)
+	// .exchange()
+	// .block();
+	// assertThat(response.statusCode().is2xxSuccessful()).isTrue();
+	// }).matchesRegex(".*\\{\"response\":\"ping\"\\}")));
+	// }
 
 }
--- stream-applications-integration-tests/src/test/java/org/springframework/cloud/stream/apps/integration/test/sink/TcpSinkTests.java
+++ stream-applications-integration-tests/src/test/java/org/springframework/cloud/stream/apps/integration/test/sink/TcpSinkTests.java
@@ -16,77 +16,58 @@
 
 package org.springframework.cloud.stream.apps.integration.test.sink;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.InetAddress;
-import java.net.ServerSocket;
-import java.net.Socket;
-import java.time.Duration;
-import java.util.concurrent.atomic.AtomicBoolean;
-
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
-import org.testcontainers.junit.jupiter.Container;
-import reactor.core.publisher.Mono;
-
-import org.springframework.cloud.stream.app.test.integration.StreamApps;
 import org.springframework.cloud.stream.apps.integration.test.support.KafkaStreamIntegrationTestSupport;
-import org.springframework.http.MediaType;
-import org.springframework.web.reactive.function.client.ClientResponse;
-import org.springframework.web.reactive.function.client.WebClient;
-
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.awaitility.Awaitility.await;
-import static org.springframework.cloud.stream.app.test.integration.kafka.KafkaStreamApps.kafkaStreamApps;
 
 public class TcpSinkTests extends KafkaStreamIntegrationTestSupport {
 
-//	private static final int port = findAvailablePort();
-//
-//	private static final int tcpPort = findAvailablePort();
-//
-//	private static Socket socket;
-//
-//	private static final AtomicBoolean socketReady = new AtomicBoolean();
-//
-//	private static WebClient webClient = WebClient.builder().build();
-//
-//	@Container
-//	private static StreamApps streamApps = kafkaStreamApps(TcpSinkTests.class.getSimpleName(), kafka)
-//			.withSourceContainer(httpSource(port))
-//			.withSinkContainer(defaultKafkaContainerFor("tcp-sink")
-//					.withEnv("TCP_CONSUMER_HOST", localHostAddress())
-//					.withEnv("TCP_PORT", String.valueOf(tcpPort))
-//					.withEnv("TCP_CONSUMER_ENCODER", "CRLF"))
-//			.build();
-//
-//	@BeforeAll
-//	static void startTcpServer() {
-//		new Thread(() -> {
-//			try {
-//				socket = new ServerSocket(tcpPort, 50, InetAddress.getLocalHost()).accept();
-//				socketReady.set(true);
-//			}
-//			catch (IOException exception) {
-//				exception.printStackTrace();
-//			}
-//		}).start();
-//	}
-//
-//	@Test
-//	void postData() throws IOException {
-//		String text = "Hello, world!";
-//		ClientResponse response = webClient
-//				.post()
-//				.uri("http://localhost:" + streamApps.sourceContainer().getMappedPort(port))
-//				.contentType(MediaType.TEXT_PLAIN)
-//				.body(Mono.just(text), String.class)
-//				.exchange()
-//				.block();
-//		assertThat(response.statusCode().is2xxSuccessful()).isTrue();
-//		await().atMost(Duration.ofSeconds(10)).untilTrue(socketReady);
-//		BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-//		await().atMost(Duration.ofSeconds(10)).until(() -> reader.readLine().equals("Hello, world!"));
-//	}
+	// private static final int port = findAvailablePort();
+	//
+	// private static final int tcpPort = findAvailablePort();
+	//
+	// private static Socket socket;
+	//
+	// private static final AtomicBoolean socketReady = new AtomicBoolean();
+	//
+	// private static WebClient webClient = WebClient.builder().build();
+	//
+	// @Container
+	// private static StreamApps streamApps =
+	// kafkaStreamApps(TcpSinkTests.class.getSimpleName(), kafka)
+	// .withSourceContainer(httpSource(port))
+	// .withSinkContainer(defaultKafkaContainerFor("tcp-sink")
+	// .withEnv("TCP_CONSUMER_HOST", localHostAddress())
+	// .withEnv("TCP_PORT", String.valueOf(tcpPort))
+	// .withEnv("TCP_CONSUMER_ENCODER", "CRLF"))
+	// .build();
+	//
+	// @BeforeAll
+	// static void startTcpServer() {
+	// new Thread(() -> {
+	// try {
+	// socket = new ServerSocket(tcpPort, 50, InetAddress.getLocalHost()).accept();
+	// socketReady.set(true);
+	// }
+	// catch (IOException exception) {
+	// exception.printStackTrace();
+	// }
+	// }).start();
+	// }
+	//
+	// @Test
+	// void postData() throws IOException {
+	// String text = "Hello, world!";
+	// ClientResponse response = webClient
+	// .post()
+	// .uri("http://localhost:" + streamApps.sourceContainer().getMappedPort(port))
+	// .contentType(MediaType.TEXT_PLAIN)
+	// .body(Mono.just(text), String.class)
+	// .exchange()
+	// .block();
+	// assertThat(response.statusCode().is2xxSuccessful()).isTrue();
+	// await().atMost(Duration.ofSeconds(10)).untilTrue(socketReady);
+	// BufferedReader reader = new BufferedReader(new
+	// InputStreamReader(socket.getInputStream()));
+	// await().atMost(Duration.ofSeconds(10)).until(() -> reader.readLine().equals("Hello,
+	// world!"));
+	// }
 }
