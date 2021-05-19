28f48d41df1d3c4eb4555cf6e66f6b6807da756f
David Turanski <dturanski@vmware.com>
2021-05-19 10:31:41 -0400

    Change default docker version to latest

--- stream-applications-integration-tests/src/test/java/org/springframework/cloud/stream/apps/integration/test/common/Configuration.java
+++ stream-applications-integration-tests/src/test/java/org/springframework/cloud/stream/apps/integration/test/common/Configuration.java
@@ -28,7 +28,7 @@ public abstract class Configuration {
 	private static final String SPRING_CLOUD_STREAM_APPLICATIONS_VERSION = "spring.cloud.stream.applications.version";
 
 	static {
-		VERSION = System.getProperty(SPRING_CLOUD_STREAM_APPLICATIONS_VERSION, "3.0.0-SNAPSHOT");
+		VERSION = System.getProperty(SPRING_CLOUD_STREAM_APPLICATIONS_VERSION, "latest");
 	}
 
 	public static class VersionSupplier implements Supplier<String> {
